a055c0d5cd9d7d8c190898ae35abf8ce589287f6
elinaviksne <elina@gmail.com>
2024-11-22 10:25:18 +0200

    first commit

---
+++ basics.js
@@ -0,0 +1,18 @@
+let personAge = 17;
+if (personAge >= 18) {
+    console.log("Pilngadīgs!")
+} else {
+    console.log("Nepilngadīgs!")
+};
+const namesArray = ["Jānis", "Anna", "Zaiga"];
+
+
+for (let i = 0; i < namesArray; i++) {
+    console.log(namesArray[i])
+};
+
+function sayHello() {
+    console.log("Labdien, ", namesArray[1])
+};
+sayHello()
+
